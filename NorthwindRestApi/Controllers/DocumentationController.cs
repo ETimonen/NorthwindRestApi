c17094a1b22b662938939179db32fc7dc8488e0c
Eero Timonen <eero.timonen@student.careeria.fi>
2025-02-05 15:32:00 +0200

    dokumentaatio-controllerin lisäys

---
+++ NorthwindRestApi/Controllers/DocumentationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindRestApi.Models;
+
+namespace NorthwindRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentationController : ControllerBase
+    {
+        // Alustetaan tietokantayhteys
+        NorthwindOriginalContext db = new NorthwindOriginalContext();
+        // Dokumentaation haku
+        [HttpGet("{id}")]
+        public ActionResult SearchDoc(int id)
+        {
+            try
+            {
+                var dokumentti = db.Documentations.Find(id);
+                if (dokumentti != null)
+                {
+                    return Ok(dokumentti);
+                }
+                else
+                {
+                    return NotFound(DateTime.Now + ": Documentation missing.");
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+    }
+}
