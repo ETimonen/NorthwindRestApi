bdfbf70a879a5f6309f6d114c703dc8fcf62955b
Eero Timonen <eero.timonen@student.careeria.fi>
2025-02-05 14:31:29 +0200

    Lisäystoiminto

--- NorthwindRestApi/Controllers/CustomersController.cs
+++ NorthwindRestApi/Controllers/CustomersController.cs
@@ -15,8 +15,53 @@ namespace NorthwindRestApi.Controllers
         [HttpGet]
         public ActionResult GetAllCustomers()
         {
-            var asiakkaat = db.Customers.ToList();
-            return Ok(asiakkaat);
+            try
+            {
+                var asiakkaat = db.Customers.ToList();
+                return Ok(asiakkaat);
+            }
+            catch(Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+            }
+        }
+
+        // Hakee yhden asiakkaan id:n perusteella
+        [HttpGet("{id}")]
+        public ActionResult GetOneCustomerById(string id)
+        {
+            try
+            {
+                var asiakas = db.Customers.Find(id);
+                if (asiakas != null)
+                {
+                    return Ok(asiakas);
+                }
+                else
+                {
+                    return NotFound($"Asiakasta id:llä {id} ei löydy.");
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e);
+            }
+        }
+
+        // Uuden asiakkaan lisääminen
+        [HttpPost]
+        public ActionResult AddNew([FromBody] Customer cust) 
+        {
+            try
+            {
+                db.Customers.Add(cust);
+                db.SaveChanges();
+                return Ok($"Lisättiin uusi asiakas {cust.CompanyName} from {cust.City}");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + e.InnerException);
+            }
         }
     }
 }
